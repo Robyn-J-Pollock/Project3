b4cefea76e650f0cffab24da78c8aa63e42b2fe1
Robyn Pollock <pollock.robyn.j@gmail.com>
2019-10-19 10:32:17 -0500

    Added comment descriptions to each of the methods in DateTimeOne.java

--- Project3/README.md
+++ Project3/README.md
@@ -4,3 +4,4 @@ Task 2: Look through Main.java for details
 Task 3: Add Abstract classes to DateTimeOne.java
 Task 4: Add JUnit test for DateTimeOne.java
 Task 5: Create tests for all DateTimeOne methods
+Task 6: Find and comment about what each method in dateTimeOne.java is supposed to return
--- Project3/src/DateTimeOne.java
+++ Project3/src/DateTimeOne.java
@@ -1,37 +1,85 @@
 
 public class DateTimeOne extends MesoDateTimeOneAbstract
 {
-
+	/*
+	 * Returns value of the current second
+	 */
 	@Override
 	int getValueOfSecond() {
 		// TODO Auto-generated method stub
 		return 0;
 	}
-
+	
+	/*
+	 * Prints value of the current time with value
+	 * "Current Date/Time: 10/08/2019 03:03 PM"
+	 */
 	@Override
 	void dateTimeNow() {
 		// TODO Auto-generated method stub
 		
 	}
-
+	
+	/*
+	 * 
+	 */
 	@Override
 	void sleepForFiveSec() {
 		// TODO Auto-generated method stub
 		
 	}
-
+	
+	/*
+	 * Prints the current time for other time-zones, in the format
+	 * "Time on Server: 15:14
+	 *  GMT: 20:14
+	 *  BST (90E): 2:14
+	 *  CST (90W): 15:14"
+	 * With GMT == GMT
+	 * BST == GMT + 6
+	 * CST == GMT - 5
+	 */
 	@Override
 	void dateTimeOfOtherCity() {
 		// TODO Auto-generated method stub
 		
 	}
 
+	/*
+	 * Stores the values of GMT, BST, and CST in a HashMap
+	 * prints them in the format 
+	 * "GMT: 10/08/2019 20:26
+	 *  BST: 10/09/2019 02:26
+	 *  CST: 10/08/2019 15:26"
+	 */
 	@Override
 	void dateTimeDifferentZone() {
 		// TODO Auto-generated method stub
 		
 	}
 
+	/*
+	 * Stores the values of GMT, BST, CST, AST, and ZST in a HashMap
+	 * Prints them in the format
+	 * "Print Style 1:
+		AST 10/01/2020 19:59
+		BST 10/09/2019 02:48
+		CST 10/08/2019 15:48
+		GMT 10/08/2019 20:48
+		ZST 11/05/2018 19:59
+		Print Style 3:
+		10/01/2020 19:59
+		10/08/2019 15:48
+		10/08/2019 20:48
+		10/09/2019 02:48
+		11/05/2018 19:59
+		Print Style 5: Final sorted Array:
+		2020-10-01T19:59
+		2019-10-09T02:48
+		2019-10-08T20:48
+		2019-10-08T15:48
+		2018-11-05T19:59"
+	 */
 	@Override
 	void timeZoneHashMap() {
 		// TODO Auto-generated method stub
