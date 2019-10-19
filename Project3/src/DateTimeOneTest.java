4e797113e3d5c59360962fada9b1addeb026e133
Robyn Pollock <pollock.robyn.j@gmail.com>
2019-10-19 10:16:28 -0500

    Created TestMethods for each method of DateTimeOneTest.java

--- Project3/src/DateTimeOneTest.java
+++ Project3/src/DateTimeOneTest.java
@@ -5,8 +5,33 @@ import org.junit.Test;
 public class DateTimeOneTest {
 
 	@Test
-	public void test() {
+	public void testGetValueOfSecond() {
 		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void testDateTimeNow() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void testSleepForFiveSec() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void testDateTimeOfOtherCity() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void testDateTimeDifferentZone() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void testTimeZoneHashMap() {
+		
 	}
 
 }
